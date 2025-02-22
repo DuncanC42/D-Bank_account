3b77872399b436637b4f99454931b3504def0f5c
shi2gami42 <cadoret.du@gmail.com>
2025-02-22 23:59:41 +0100

    feign client implementation part 1

---
+++ src/main/java/bzh/duncan/accounts/controller/CustomerController.java
@@ -0,0 +1,49 @@
+package bzh.duncan.accounts.controller;
+
+import bzh.duncan.accounts.dto.CustomerDetailsDto;
+import bzh.duncan.accounts.dto.ErrorResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.Pattern;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(
+        name = "Rest api for customer",
+        description = "REST api to fetch customer details"
+)
+@RestController
+@RequestMapping(value = "/api", produces = "application/json")
+@Validated
+public class CustomerController {
+
+    @Operation(
+            summary = "fetch customer details",
+            description = "fetch customer details by mobile number"
+    )
+    @ApiResponses
+            (value = {
+                    @ApiResponse(responseCode = "200",
+                            description = "Customer details fetched successfully"
+                    ),
+                    @ApiResponse(responseCode = "500",
+                            description = "Internal server error",
+                            content = @Content(
+                                    schema = @Schema(implementation = ErrorResponseDto.class)
+                            )
+                    )
+            })
+    @GetMapping(value = "/fetchCustomerDetails")
+    public ResponseEntity<CustomerDetailsDto> fetchCustomerDetails(
+            @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number should be 10 digits")
+            String mobileNumber) {
+        return null;
+    }
+}
---
+++ src/main/java/bzh/duncan/accounts/service/client/LoansFeignClient.java
@@ -0,0 +1,17 @@
+package bzh.duncan.accounts.service.client;
+
+import bzh.duncan.accounts.dto.CardsDto;
+import bzh.duncan.accounts.dto.LoansDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "cards")
+public interface LoansFeignClient {
+
+
+    @GetMapping(value = "/api/fetch", consumes = "application/json")
+    public ResponseEntity<LoansDto> fetchLoanDetails(@RequestParam String mobileNumber);
+
+}
